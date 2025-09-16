bea6840f6c2d8fa1e5c1a59f55b77d1d954ab52f
tatianno <tferreiraalves@gmail.com>
2025-09-16 11:38:06 -0300

    Atualiza a versão do pacote para 0.2.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r") as arq:
 
 setup(
     name='glpi-provider',
-    version='0.2.4',
+    version='0.2.5',
     license='MIT License',
     author='Tatianno Alves',
     long_description=readme,
