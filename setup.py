b892dc2846f1fd48b3de717eefec1ae33afb5768
tatianno <tferreiraalves@gmail.com>
2026-04-06 11:08:47 -0300

    Atualiza versão do pacote para 0.3.8

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r") as arq:
 
 setup(
     name='glpi-provider',
-    version='0.3.7',
+    version='0.3.8',
     license='MIT License',
     author='Tatianno Alves',
     long_description=readme,
